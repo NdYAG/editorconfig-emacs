277a09942d309bfb43963129d0496bb87563e6d8
10sr <8slashes+git@gmail.com>
2016-02-14 02:23:39 +0900

    WIP

--- ert-tests/editorconfig.el
+++ ert-tests/editorconfig.el
@@ -12,3 +12,20 @@
 (ert-deftest has-feature-01 nil
   "minimally working - provides 'editorconfig"
   (should (featurep 'editorconfig)))
+
+(ert-deftest test-apply-when-find-file ()
+  "Test if apply called when visiting file."
+  (editorconfig-mode 1)
+  (prefer-coding-system 'utf-8-unix)
+  (let ((files-dir (concat default-directory
+                     "plugin-test/test_files/")))
+    (dolist (target-charset '(
+                               ("latin1.txt" . iso-latin-1-unix)
+                               ("utf-8.txt" . utf-8-unix)))
+      (let ((target (concat files-dir
+                      (car target-charset))))
+        (with-current-buffer (find-file-noselect target)
+          (should (eq buffer-file-coding-system
+                    (cdr target-charset))))))
+
+  ))
